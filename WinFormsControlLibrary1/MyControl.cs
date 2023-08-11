40e68431e50dbe3ef6c544d8ad858a005008e5d4
dmitrybv <dimabol@yandex.ru>
2023-08-11 14:41:26 +0300

    Add project files.

---
+++ WinFormsControlLibrary1/MyControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsControlLibrary1
+{
+
+  [Designer(typeof(MyControlDesigner))]
+  [ToolboxItem(true)]
+  public class MyControl : Control 
+  {
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+      e.Graphics.FillRectangle(SystemBrushes.Info, new Rectangle(0, 0, Width, Height));
+      e.Graphics.DrawString("MyControl", Font, SystemBrushes.InfoText, new PointF(0, 0));
+    }
+  }
+}
---
+++ WinFormsControlLibrary1/MyControlDesigner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+
+namespace WinFormsControlLibrary1
+{
+  internal class MyControlDesigner : ControlDesigner
+  {
+    private DesignerActionListCollection actionLists;
+
+    public override DesignerActionListCollection ActionLists
+    {
+      get
+      {
+        if (actionLists == null)
+        {
+          actionLists = new DesignerActionListCollection();
+          actionLists.Add(new DataAxisGridActionList(Component));
+          actionLists.AddRange(base.ActionLists);
+        }
+        return actionLists;
+      }
+    }
+  }
+
+  public class DataAxisGridActionList : DesignerActionList
+  {
+
+    public DataAxisGridActionList(IComponent component) : base(component)
+    {
+    }
+
+    public override DesignerActionItemCollection GetSortedActionItems()
+    {
+      DesignerActionItemCollection items = new DesignerActionItemCollection();
+      items.Add(new DesignerActionMethodItem(this, "Action1", " Action 1", true));
+      items.Add(new DesignerActionMethodItem(this, "Action2", "Action 2", true));
+      return items;
+    }
+
+    public void Action1()
+    {
+      MessageBox.Show("Action 1");
+    }
+
+    public void Action2()
+    {
+      MessageBox.Show("Action 2");
+    }
+  }
+
+}
